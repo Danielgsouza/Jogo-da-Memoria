e972f4c47595b7fa3a6802c26da1a9172877858c
Daniel Souza <danielgsouza4@gmail.com>
2021-11-01 13:01:41 -0300

    revisao2

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,7 @@ let game = {
     firstCard: null,
     secondCard: null,
     
-    techs : ['bootstrap','css', 'electron', 'firebase', 'html', 'Javascript', 'jquery', 'mongo', 'node', 'react'], 
+    techs : ['bootstrap','css', 'electron', 'firebase', 'html', 'javascript', 'jquery', 'mongo', 'node', 'react'], 
     cards : null, 
 
     setCard: function(id) {
